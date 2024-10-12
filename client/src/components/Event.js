6b987a410a9c056800e116b636069cefc6f1f561
QuachCongThinh <qcthinh3105@gmail.com>
2024-10-12 15:25:56 +0700

    Update Code

--- client/src/components/Event.js
+++ client/src/components/Event.js
@@ -1,4 +1,9 @@
 const Event = () => {
+  const handleMapClick = (latitude, longitude) => {
+    const mapUrl = `https://www.google.com/maps?q=${latitude},${longitude}`;
+    window.open(mapUrl, "_blank");
+  };
+
   return (
     <div id="event-section" className="event">
       <h1>Sự kiện cưới</h1>
@@ -8,9 +13,23 @@ const Event = () => {
       </p>
       <div className="event-wedding">
         <div className="event-male">
-          
+          <div className="thumb-male"></div>
+          <h1>Tiệc cưới nhà Nam</h1>
+          <p>15:00 01/01/2025</p>
+          <h2>Cái Nước</h2>
+          <button onClick={() => handleMapClick(8.937824, 105.016566)}>
+            Xem bản đồ
+          </button>
         </div>
-        <div className="event-female"></div>
+        <div className="event-female">
+          <div className="thumb-female"></div>
+          <h1>Tiệc cưới nhà Nữ</h1>
+          <p>15:00 01/01/2025</p>
+          <h2>Đông Hưng</h2>
+          <button onClick={() => handleMapClick(8.95718, 105.098513)}>
+            Xem bản đồ
+          </button>
+        </div>
       </div>
     </div>
   );
