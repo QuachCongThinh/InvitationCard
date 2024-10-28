7ede2dbc11a7bb2433469d82a0456427a97b05ef
QuachCongThinh <qcthinh3105@gmail.com>
2024-10-28 14:16:27 +0700

    UpdateCode

--- client/src/components/Event.js
+++ client/src/components/Event.js
@@ -1,6 +1,4 @@
 import React, { useEffect } from "react";
-// import roseLeft from "../assets/image/rose-left.png";
-// import roseRight from "../assets/image/rose-right.png";
 
 const Event = () => {
   const handleMapClick = (latitude, longitude) => {
@@ -8,51 +6,8 @@ const Event = () => {
     window.open(mapUrl, "_blank");
   };
 
-  // useEffect(() => {
-  //   const roseLeft = document.querySelector(".roseLeft");
-
-  //   const handleScroll = () => {
-  //     const rect = roseLeft.getBoundingClientRect();
-  //     const isVisibleLeft = rect.top > 100 && rect.bottom >= window.innerHeight;
-
-  //     if (isVisibleLeft) {
-  //       roseLeft.classList.add("open");
-  //     } else {
-  //       roseLeft.classList.remove("open");
-  //     }
-  //   };
-
-  //   window.addEventListener("scroll", handleScroll);
-  //   return () => {
-  //     window.removeEventListener("scroll", handleScroll);
-  //   };
-  // }, []);
-
-  // useEffect(() => {
-  //   const roseRight = document.querySelector(".roseRight");
-  //   const handleScroll = () => {
-  //     const rectRight = roseRight.getBoundingClientRect();
-  //     const isVisibleRight =
-  //       rectRight.top < 1100 && rectRight.bottom <= window.innerHeight;
-
-  //     if (isVisibleRight) {
-  //       roseRight.classList.add("open");
-  //     } else {
-  //       roseRight.classList.remove("open");
-  //     }
-  //   };
-  //   window.addEventListener("scroll", handleScroll);
-  //   return () => {
-  //     window.removeEventListener("scroll", handleScroll);
-  //   };
-  // }, []);
-
   return (
     <div id="event-section" className="event">
-      {/* <div className="rose-container">
-        <img src={roseLeft} alt="Rose" className="roseLeft" />
-        <img src={roseRight} alt="Rose" className="roseRight" />
-      </div> */}
       <h1>Sự kiện cưới</h1>
       <p>
         Tình yêu không làm cho thế giới quay tròn. Tình yêu là những gì làm cho
